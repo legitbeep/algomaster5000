29eaa3dfe29e241c81736c71f8cc99f478e36f4f
Krishna <krishnajalan@jklu.edu.in>
2020-09-30 16:36:19 +0530

    Algomaster5000

---
+++ command.py
@@ -0,0 +1,41 @@
+from tkinter import END, INSERT
+
+sym = {'{': '}',
+		'[':']',
+		'(':')', 
+		'numofTab': 0,
+		'last': "1"}
+
+def choose_color(): 
+  
+    # variable to store hexadecimal code of color 
+    color_code = colorchooser.askcolor(title ="Choose color")  
+    print(color_code)
+
+def saveText(editor):
+	StringResult = editor.get(1.0, 'end-1c')
+	print(StringResult)
+
+
+def completeSymbols(editor, c):
+	editor.insert(END, sym[c])
+
+
+def completeIndentation(editor):
+	editor.insert('end-1c', sym['numofTab']*'\t')
+
+
+def getKey(e, editor):
+
+	print(e.char, sym['numofTab'], sym['last'])
+	sym['last'] = editor.get(END)
+
+	if sym['last'] == e.char and sym['last'] in sym.items():
+		editor.delete(END)
+		sym['last'] = "1"
+
+	if e.char == chr(13):
+		completeIndentation(editor)
+	if e.char in sym:
+		completeSymbols(editor, e.char)
+		sym['numofTab'] += 1
