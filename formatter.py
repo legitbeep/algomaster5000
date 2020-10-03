cb36498161a0ad7b4503f76fcbf839f135f3cf2d
krishna <krishnajalan@jklu.edu.in>
2020-10-04 02:10:27 +0530

    code extractor

--- formatter.py
+++ formatter.py
@@ -1,8 +1,26 @@
 import os
-import subprocess
 import black
 
+class CodeExtractor:
+        
+        def __init__(self, file, stringFormat = False):
+                if stringFormat:
+                        __StringtoCode(self, code)
+                        Formatter("flowchart$algomaster.py")
+                else:
+                        Formatter(file)
 
-class formatter:
+        def __StringtoCode__(self, code):
+                file = open("flowchart$algomaster.py", "w")
+                file.write(code)
+                file.close()
+
+        def __delete(self):
+                os.system("rm flowchart$algomaster.py")
+
+
+obj = CodeExtractor()
+
+class Formatter:
         def __init__(self, filename):
-                os.system("black "+filename)
+                os.system("black -l 1000 "+filename)
